ae534ee58ed477155548ea43ca52c410e422e65b
Alexandra Craig <alexandra.craig@gmail.com>
2016-09-16 19:29:36 -0400

    three methods working

--- mystery_word.rb
+++ mystery_word.rb
@@ -1,50 +1,68 @@
-def get_all_words(lines) # retrieves and cleans all words in file and stores in arr
-  lines = File.readlines("usr/share/dict/words")
-  lines.map do |words|
-    words.chomp
+easy = []
+normal = []
+hard = []
+
+def get_all_words(x) # retrieves and cleans all words in file and stores in arr
+  dictionary = File.readlines(x)
+  dictionary.map! do |i|
+    i.chomp
   end
 end
 
-def get_easy_words(lines)
-  easy_words = []
-  easy = lines.each |words|
-  if (words.length >= 4) && (words.length <= 6)
-    easy_words << words
+def get_easy_words(x, array)
+  x.each do |i|
+    if i.length >= 4 && i.length <= 6
+      array.push(i)
+    end
   end
 end
 
-def get_normal_words(lines)
+def get_normal_words(x, array)
+  x.each do |i|
+    if i.length >= 6 && i.length <= 8
+      array.push(i)
+    end
+  end
+end
+
+def get_hard_words(x, array)
+  x.each do |i|
+    if i.length >= 8 && i.length <= 24
+      array.push(i)
+    end
+  end
+end
+=begin
+def get_normal(x)
   normal_words = []
-  normal = lines.each |words|
-  if (words.length >= 6) && (words.length <= 8)
-    normal_words << words
+  x.each do |i|
+    if (i.length >=6 ) && (i.length <=8 )
+      normal_words << x
+    end
   end
 end
+=end
 
-def get_hard_words(lines)
-  hard_words = []
-  hard = lines.each |words|
-  if (words.length >= 8) && (words.length <= 24)
-    hard_words << words
+
+dictionary = get_all_words("/usr/share/dict/words")
+get_easy_words(dictionary,easy)
+get_normal_words(dictionary,normal)
+get_hard_words(dictionary,hard)
+print hard
+
+
+=begin
+ if dictionary.each do |x|
+  if x.length >= 4 && x.length <= 6
+    easy.push(x)
   end
 end
+=end
 
 
-def # select word at random
-end
 
-def # display word with blanks/letters filled in
-
-end
-
-def # check if word has been guessed
-
-end
-
-def main()
-  # enter main code here
-end
-
+=begin
 if __FILE__ == $PROGRAM_NAME
   main
 end
+=end
