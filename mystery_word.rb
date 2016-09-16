a47047732ee11d98c08557fe8ff7e74b7a31badf
Kristine <kteich88@gmail.com>
2016-09-16 14:13:13 -0400

    Added required functions and comments to fill in

---
+++ mystery_word.rb
@@ -0,0 +1,21 @@
+def # select subset from complete word list
+end
+
+def # select word at random
+end
+
+def # display word with blanks/letters filled in
+
+end
+
+def # check if word has been guessed
+
+end
+
+def main()
+  # enter main code here
+end
+
+if __FILE__ == $PROGRAM_NAME
+  main
+end
